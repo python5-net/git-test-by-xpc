c5ba8c2bd45fd8765e9e756520f85813132c4572
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-20 10:27:08 +0800

    测试sh命令行语句

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,18 @@
 pipeline {
-    agent any  
+    agent {
+	label 'xxx-agent-机器FirstPipeLine项目测试'
+	customWorkspace "${env.JOB_NAME}/${env.BUILD_NUMBER}"
+    }  
+
+
     stages {
 
         stage('Build') {
             steps {
-                println "Build项目开始了by——xpc" 
+                println "Build项目开始了by——xpc,下面开始执行shell的命令语句"
+		sh "pwd" 
+		println "打印出了当前所在的目录路径"
+			
             }
      }
 
@@ -12,14 +20,16 @@ pipeline {
         stage('Test') { 
             steps {
                 println "Test" 
-            }
+            	sh "echo ${JAVA_HOME} "
+		println "上面已经打印出来了java的执行路径java_HOme"
+	    }
         }
     
 
     stage('Deploy') { 
             steps {
-                println "Deploy 测试输出env...."
-		println "${env.JOB_NAME}/${env.BUILD_NUMBER}"
+                println "Deploy 测试输出env.... 下面将没有输出语句 应该直接是succed等语句"
+		// println "${env.JOB_NAME}/${env.BUILD_NUMBER}"
             }
         }
 
