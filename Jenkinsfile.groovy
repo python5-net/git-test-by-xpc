a7defbf470fafe38b7c76c4c8bd369d7bdb04291
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 15:06:15 +0800

    commit提交重新构建步骤

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,21 @@
 Pipeline {
     agent any
-   
+    
     stages {
         stage('Example') {
-    	steps{
+    		steps{
 	println 'example is running '
-
+	}
+	}
+	stage('test'){
+		steps{
+	println "testis runing "
+		}
+	}
+	
+	stage('deploy'){
+		steps{
+			println "deploy is running "
 	}
 	}
     }
