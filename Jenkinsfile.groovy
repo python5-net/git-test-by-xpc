50a6fb9df835ac47f661aaecdc297cbae1af9596
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 14:11:09 +0800

    。。。

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,22 @@
 Pipeline {
     agent any
-    environment {
-        unit_test = true
-    }
-    stages {
-        stage('Example') {
-            steps {
-                if(unit_test == true) {
-                   println "检测到weitrue的条件"
-		   // call run unit test methods
-                }
-            }
-        }
-    }
+
+	stages{
+		stage('example--'){
+		steps{
+			println "hello example"
+		}
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
 }
