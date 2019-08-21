a2ac535f4e1ef6083cade328858452d9b92d27c0
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 15:12:51 +0800

    再次.....

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,9 @@
 pipeline {
     agent any
+   enviroment{
+	unit_test=true
+   }
+
     stages {
         stage('Example') {
     		steps{
@@ -9,6 +13,11 @@ pipeline {
 	stage('test'){
 		steps{
 	println "testis runing "
+	if(unit_test==true){
+	println "判断为true的语句再次执行成功，下面开始测试执行ssh语句"
+	sh "ll -lha"
+	}
+
 		}
 	}
 	
