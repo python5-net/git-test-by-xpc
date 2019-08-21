2b1847756756bcf5474da8366f897d48b2131530
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 14:56:07 +0800

    恢复测试001

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,10 @@
 Pipeline {
     agent any
-    environment {
-        unit_test = true
-    }
+   
     stages {
         stage('Example') {
             steps {
-                if(unit_test == true) {
-                   // call run unit test methods
-                }
+                println "hello example "
             }
         }
     }
