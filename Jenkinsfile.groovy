125fe57623b9edec58efa0d45d1b7073ceaf201c
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 14:08:10 +0800

    测试if条件是否执行

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,16 @@
-Pipeline{
-
-
-	agent any
-
-	environment{
-		unit_test=true
-	}
-
-	stages{
-		
-		stage('Build'){
-			steps{
-			println "输出了build的语句~~~"
-			}
-
-		}
-
-
-		stage('Example_xpc'){
-			steps{
-				if (unit_test == true){
-					println "这是如果单元测试为true输出的语句"
-					sh 'll -lha'
-					println "上面输出了所在目录的所有文件和详细的说明"
-
-				}
-			}
-		}
-	}
-
-
-
+Pipeline {
+    agent any
+    environment {
+        unit_test = true
+    }
+    stages {
+        stage('Example') {
+            steps {
+                if(unit_test == true) {
+                   println "检测到weitrue的条件"
+		   // call run unit test methods
+                }
+            }
+        }
+    }
 }
