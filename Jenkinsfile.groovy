69e58828c35c2744ce9733b0e041a57335995ae5
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 15:24:09 +0800

    添加script

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,11 +13,13 @@ pipeline {
 	stage('test'){
 		steps{
 	println "testis runing "
-	if(unit_test == true){
 	println "判断为true的语句再次执行成功，下面开始测试执行ssh语句"
 	sh "ll -lha"
-	}
-
+		}
+		script{
+		if(unit_test == true){
+			println "测试if or else "
+		}
 		}
 	}
 	
