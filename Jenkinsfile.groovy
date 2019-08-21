1ee7f4a588ac0d23e6fed6a8caf4b0206d9ad8bd
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-21 15:18:46 +0800

    继续测试if or true

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
    enviroment{
-	unit_test=true
+	unit_test = true
    }
 
     stages {
@@ -13,7 +13,7 @@ pipeline {
 	stage('test'){
 		steps{
 	println "testis runing "
-	if(unit_test==true){
+	if(unit_test == true){
 	println "判断为true的语句再次执行成功，下面开始测试执行ssh语句"
 	sh "ll -lha"
 	}
