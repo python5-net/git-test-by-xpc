ed9a9f3f451d727c43a630b413006eeb33c254cc
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-22 09:29:51 +0800

    再次添加条件if

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,25 +5,25 @@ pipeline {
    }
 
     stages {
-        stage('Example') {
+        
+	stage('Example') {
     		steps{
 	println 'example is running '
 	}
 	}
+
 	stage('test'){
-		steps{
-	println "testis runing "
-	println "判断为true的语句再次执行成功，下面开始测试执行ssh语句"
-		}
-		if(unit_test==true){
-		println "condition"
-		}
+	if (unit_test==true){
+		println "hello if。。。"
 	}
-	
+
+	}
+
 	stage('deploy'){
 		steps{
 		println "deploy is running "
 	}
 	}
+
     }
 }
