a61dc3a9b18e9ac38ee6a7d5eba8d82fce9549be
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-22 13:18:55 +0800

    添加遗漏的英文逗号

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
 	xpc='我是xpc--===---==-=-=-=-=--=-=-=-=-'
    }
     parameters{
-	string(name:'userName',defaultValue:'xpcName'description:'please give me a name ')
+	string(name:'userName',defaultValue:'xpcName',description:'please give me a name ')
 	choice(name:'version',choices:['1.1','1.2','1.3'],description:'select the version to test')
 	booleanParam(name:'is_boy',defaultValue:true,description:'you are boy or not ')
     }
