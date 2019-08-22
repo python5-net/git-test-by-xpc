962754ccf976040d5a808a1d4f2d514deb9c17df
pengchengXu-suez <pengcheng.xu@suez.com>
2019-08-22 17:05:30 +0800

    测试if语句 flowcontrol

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,44 +1,23 @@
-pipeline {
-    agent any
-   environment{
-	unit_test = false
-	xpc='我是xpc--===---==-=-=-=-=--=-=-=-=-'
-   }
-  //  parameters{
- //	string(name:'userName',defaultValue:'xpcName',description:'please give me a name ')
-//	choice(name:'version',choices:['1.1','1.2','1.3'],description:'select the version to test')
-//	booleanParam(name:'is_boy',defaultValue:true,description:'you are boy or not ')
-//  }
-    stages{
-	stage('test'){
-		steps{
-			script{
-				sh 'java -version'
-				println 'hello 脚本式pipe构建 删除了sh的ll命令'
-			}
-		}
-	}
-	
-	stage('Example if'){
-		steps{
-			// if(unit_test==true){
-			// call run unit test
-			//}
-			println '代替删除了if语句'
-		}
-	}
-	
-	stage('deploy xpc Demo'){
+pipeline{
+
+	agent any
+	stages{
 		
-		when{
-			expression{
-				return (unit_test)
-			}
+		stage('flow control'){
+		    steps{
+			script{
+			    if (10==10){
+			    println "pass (～￣▽￣)～了"
+			    }else{
+			    println "failed ε=(´ο｀*)))唉"
+			    }
+		        }
+		    }
 		}
-		steps{
-			echo 'unit_test==true 的执行语句'
-		}
-         }
-    }
+	}
+
+
+
+
 
 }
