c30d5b1264c2449adb313f1f63e567147b6f98f3
pengchengXu-suez <pengcheng.xu@suez.com>
2019-09-05 17:02:49 +0800

    copy jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,10 @@
-
 pipeline{
 
 	agent any
 	stages{
 		
+		stage('flow control'){
+		    steps{
 			script{
 			    if (10==10){
 			    println "pass (～￣▽￣)～了"
@@ -27,30 +28,13 @@ pipeline{
 				}		
 		}
 		
-		stage('测试切换目录'){
+		stage('测试ssh远程操作command'){
 			steps{
-				dir('../test/'){
+				sh('ssh 10.1.1.6 " mkdir 10116-file;echo 远程创建了一个文件;ls -lha > xpc.txt"')
 				
-				sh('ls -lha ')
-				sh('echo 将执行清除当前目录的所有的文件')
-				deleteDir()
-				sh 'ls -lha'
-
-					}
-				sh 'pwd'
-				sh("ls -al ${env.WORKSPACE}")
-				println '回到了原来工作路径了？吗'
 			}
 		}
 		
-		stage('改为测试bash命令	'){
-			steps{
-				
-				sh ( 'ssh 10.1.1.6 " mkdir 10116-file;echo 远程创建了一个文件;ls -lha > xpc.txt'  )
-	
-
-				}
-			}
 
 
 
