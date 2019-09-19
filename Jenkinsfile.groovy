34b3a362986367875944f1904e9ed4ff2a249a66
pengchengXu-suez <pengcheng.xu@suez.com>
2019-09-19 17:09:36 +0800

    commit finally ？

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -30,7 +30,7 @@ pipeline {
                 subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                 body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
                     <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
-                to: "user1@qq.com,user2@qq.com",
+                to: "pengcheng.xu@suez.com",
                 from: "admin@sina.com"
             )
         }
